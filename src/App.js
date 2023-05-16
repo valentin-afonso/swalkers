7a9f3b375f6abc44e3465d0e6db9d38ce57ba66c
Valentin Afonso <valentinafonso22@gmail.com>
2023-05-16 19:47:33 +0200

    ✨ feature: add searchbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './images/svg/logo.svg';
 import './App.css';
 import Map from './components/Map';
 import Filters from './components/Filters';
+import Searchbar from './components/Searchbar';
 
 function App() {
   return (
     <div className="home">
       <header className="header">
         <img src={logo} className="logo" alt="logo" />
+        <Searchbar />
         <Filters />
       </header>
       <Map />
---
+++ src/components/Searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style/Searchbar.css'
+
+function Searchbar() {
+  return (
+    <div className='searchbar'>
+        <input type="text" placeholder='Ou allez-vous ?' />
+    </div>
+  )
+}
+
+export default Searchbar
