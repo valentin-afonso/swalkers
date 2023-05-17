0cc144e9bd8a2d07e41e466f4bdb99fc2de26c0c
Valentin Afonso <valentinafonso22@gmail.com>
2023-05-17 15:05:14 +0200

    ✨feature: add custom marker me

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 
+
+import L from 'leaflet';
+
+
 function LocationMarker({ position }) {
+  const icon = new L.Icon({
+    iconUrl: require('../images/marker-me.png'),
+    iconRetinaUrl: require('../images/marker-me.png'),
+    iconAnchor: null,
+    popupAnchor: true,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: [86, 86],
+    className: 'marker-me'
+});
   return position ? (
-    <Marker position={position}>
+    <Marker position={position} icon={icon}>
       <Popup>You are here</Popup>
     </Marker>
   ) : null;
